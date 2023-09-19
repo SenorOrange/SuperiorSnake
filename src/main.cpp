f6302eba5767992763b87f9d0cbbbdcb66fe44dd
Senor Orange <joshmoller7@gmail.com>
2023-09-19 16:46:58 -0700

    September 19, 2023 V2

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
      Members:      Josh Moller, Henry, Dio                                       
      Created:      Tue Sep 12 2023                                           
      Description:  This is our robot for the 2023/24 OverUnder
-     competition. It has a 4 motor tank drive base.
+                   It has a 4 motor tank drive base.
                                                                               */
 
 /* ---- VEXCODE DEVICES ----
@@ -29,32 +29,21 @@ int main() {
   int deadband = 5;
 
   while (true) {
-    // Get the velocity percentage of the left motor. (Axis3)
     int leftMotorSpeed = Controller1.Axis3.position();
-    // Get the velocity percentage of the right motor. (Axis2)
     int rightMotorSpeed = Controller1.Axis2.position();
 
-    // Set the speed of the left motor. If the value is less than the deadband,
-    // set it to zero.
     if (abs(leftMotorSpeed) < deadband) {
-      // Set the speed to zero.
       LeftMotor.setVelocity(0, percent);
     } else {
-      // Set the speed to leftMotorSpeed
       LeftMotor.setVelocity(leftMotorSpeed, percent);
     }
 
-    // Set the speed of the right motor. If the value is less than the deadband,
-    // set it to zero.
     if (abs(rightMotorSpeed) < deadband) {
-      // Set the speed to zero
       RightMotor.setVelocity(0, percent);
     } else {
-      // Set the speed to rightMotorSpeed
       RightMotor.setVelocity(rightMotorSpeed, percent);
     }
 
-    // Spin both motors in the forward direction.
     LeftMotor.spin(forward);
     RightMotor.spin(forward);
 
